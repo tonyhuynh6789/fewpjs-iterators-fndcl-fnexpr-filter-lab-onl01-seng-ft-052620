d4df75f6477a94b4307925f07f39687497773ec1
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2021-07-15 16:59:41 -0700

    commit tree 322cd6998aa474ca4272fd141df5793a4a33d8bb

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-// let arr = [1,2,3,4]
-
-// let even = [];
-
-// for(var i = 0; i< arr.length; i++) {
-//     if (arr[i] % 2 === 0) even.push(arr[i]);
-// }
-
-
-// let even = arr.filter(n => {
-//     return n % 2 === 0;
-// });
-
-
 function findMatching(drivers, string) {
     return drivers.filter(driver => {return driver.toUpperCase() === string.toUpperCase()})
-}
+} 
 
 function fuzzyMatch(drivers, string) {
     return drivers.filter(driver => driver.startsWith(string))
 }
+
 
 function matchName(drivers, string) {
     return drivers.filter(driver => driver.name === string)
